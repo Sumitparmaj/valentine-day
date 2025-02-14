6c11e29747dcd0e23dff1cb6adf02f5855e2730d
Sumitparmaj <Sumitparmaj47@gmail.com>
2025-02-14 11:04:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-onload = () =>{
-        document.body.classList.remove("container");
+window.onload = () => {
+    document.body.classList.remove("container");
+
+    // Ensure message is shown
+    let message = document.createElement("div");
+    message.className = "valentine-message";
+    message.innerText = "Happy Valentine's Day Naushu";
+    document.body.appendChild(message);
 };
